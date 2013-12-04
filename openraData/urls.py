fbc13c339881d42ed1cb4ff769720e0a432326a4
Igor Popov <ihptru@gmail.com>
2013-12-04 23:53:38 +0400

    extend mapAPI; add favicon

--- openraData/urls.py
+++ openraData/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
 
 from openraData import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^map/(?P<arg>\w+)/$', views.mapAPI, name='mapAPI_download'),
-    url(r'^map/(?P<arg>\w+)/(?P<value>\w+)/$', views.mapAPI, name='mapAPI')
+    url(r'^map/(?P<arg>\w+)/(?P<value>\w+)/$', views.mapAPI, name='mapAPI'), 
+    url(r'^map/(?P<arg>\w+)/(?P<value>\w+)/(?P<filter>\w+)/$', views.mapAPI, name='mapAPI_list'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico'))
 )
--- openraData/views.py
+++ openraData/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 def index(request):
     return HttpResponse("You are at the OpenRA Content Website index page.")
 
-def mapAPI(request, arg, value=""):
+def mapAPI(request, arg, value="", filter=""):
     if arg == "title":
         pass
         # map info by title (title = value)
@@ -14,6 +14,13 @@ def mapAPI(request, arg, value=""):
     elif arg == "url":
         pass
         # map url by hash (hash = value)
+    elif arg == "minimap":
+        pass
+        # minimap.png for map in JSON format encoded into base64 (hash = value)
+    elif arg == "list":
+        pass
+        # (mod = value)
+        # filtering by rating / players / author / date
     else:
         pass
         # download map by hash (hash = arg)
