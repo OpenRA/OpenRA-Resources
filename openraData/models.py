842a9a715326dfa3b9ba14d120192f40a29ae482
Igor Popov <ihptru@gmail.com>
2013-12-18 14:37:37 +0400

    add "requires_upgrade" field to maps; will be automatically updated by weekly runs of OpenRA.Lint.exe checks of currently used OpenRA version on server; maps which actually require upgrade to match recent release vesrion should be graphically marked, email must be sent to owner and map itself must be forbidded in mapAPI

--- openraData/models.py
+++ openraData/models.py
@@ -7,99 +7,100 @@ class Maps(models.Model):
     class Meta:
         verbose_name = 'Map'
 
-    user        = models.ForeignKey(User)
-    title       = models.CharField(max_length=200)
-    description = models.CharField(max_length=400)
-    info        = models.CharField(max_length=400)
-    author      = models.CharField(max_length=40)
-    map_type    = models.CharField(max_length=40)
-    players     = models.IntegerField(default=0)
-    game_mod    = models.CharField(max_length=16)
-    map_hash    = models.CharField(max_length=40)
-    width       = models.CharField(max_length=16)
-    height      = models.CharField(max_length=16)
-    tileset     = models.CharField(max_length=16)
-    revision    = models.IntegerField(default=1)
-    pre_rev     = models.IntegerField(default=0)
-    next_rev    = models.IntegerField(default=0)
-    downloading = models.BooleanField(default=True)
-    posted      = models.DateTimeField('date published')
-    viewed      = models.IntegerField(default=0)
-    rating      = RatingField(range=5, allow_anonymous=True, use_cookies=True)
+    user                = models.ForeignKey(User)
+    title               = models.CharField(max_length=200)
+    description         = models.CharField(max_length=400)
+    info                = models.CharField(max_length=400)
+    author              = models.CharField(max_length=40)
+    map_type            = models.CharField(max_length=40)
+    players             = models.IntegerField(default=0)
+    game_mod            = models.CharField(max_length=16)
+    map_hash            = models.CharField(max_length=40)
+    width               = models.CharField(max_length=16)
+    height              = models.CharField(max_length=16)
+    tileset             = models.CharField(max_length=16)
+    revision            = models.IntegerField(default=1)
+    pre_rev             = models.IntegerField(default=0)
+    next_rev            = models.IntegerField(default=0)
+    downloading         = models.BooleanField(default=True)
+    requires_upgrade    = models.BooleanField(default=False)
+    posted              = models.DateTimeField('date published')
+    viewed              = models.IntegerField(default=0)
+    rating              = RatingField(range=5, allow_anonymous=True, use_cookies=True)
 
 class Units(models.Model):
 
     class Meta:
         verbose_name = 'Unit'
 
-    user        = models.ForeignKey(User)
-    title       = models.CharField(max_length=200)
-    info        = models.CharField(max_length=400)
-    unit_type   = models.CharField(max_length=16)
-    category    = models.CharField(max_length=16)
-    palette     = models.CharField(max_length=16)
-    revision    = models.IntegerField(default=1)
-    pre_rev     = models.IntegerField(default=0)
-    next_rev    = models.IntegerField(default=0)
-    posted      = models.DateTimeField('date published')
-    viewed      = models.IntegerField(default=0)
-    rating      = RatingField(range=5, allow_anonymous=True, use_cookies=True)
+    user                = models.ForeignKey(User)
+    title               = models.CharField(max_length=200)
+    info                = models.CharField(max_length=400)
+    unit_type           = models.CharField(max_length=16)
+    category            = models.CharField(max_length=16)
+    palette             = models.CharField(max_length=16)
+    revision            = models.IntegerField(default=1)
+    pre_rev             = models.IntegerField(default=0)
+    next_rev            = models.IntegerField(default=0)
+    posted              = models.DateTimeField('date published')
+    viewed              = models.IntegerField(default=0)
+    rating              = RatingField(range=5, allow_anonymous=True, use_cookies=True)
 
 class Mods(models.Model):
 
     class Meta:
         verbose_name = 'Mod'
 
-    user        = models.ForeignKey(User)
-    title       = models.CharField(max_length=200)
-    info        = models.CharField(max_length=2000)
-    revision    = models.IntegerField(default=1)
-    pre_rev     = models.IntegerField(default=0)
-    next_rev    = models.IntegerField(default=0)
-    posted      = models.DateTimeField('date published')
-    viewed      = models.IntegerField(default=0)
-    rating      = RatingField(range=5, allow_anonymous=True, use_cookies=True)
+    user                = models.ForeignKey(User)
+    title               = models.CharField(max_length=200)
+    info                = models.CharField(max_length=2000)
+    revision            = models.IntegerField(default=1)
+    pre_rev             = models.IntegerField(default=0)
+    next_rev            = models.IntegerField(default=0)
+    posted              = models.DateTimeField('date published')
+    viewed              = models.IntegerField(default=0)
+    rating              = RatingField(range=5, allow_anonymous=True, use_cookies=True)
 
 class Palettes(models.Model):
 
     class Meta:
         verbose_name = 'Palette'
 
-    user        = models.ForeignKey(User)
-    title       = models.CharField(max_length=200)
-    info        = models.CharField(max_length=400)
-    used        = models.IntegerField(default=0)
-    posted      = models.DateTimeField('date published')
+    user                = models.ForeignKey(User)
+    title               = models.CharField(max_length=200)
+    info                = models.CharField(max_length=400)
+    used                = models.IntegerField(default=0)
+    posted              = models.DateTimeField('date published')
 
 class Reports(models.Model):
 
     class Meta:
         verbose_name = 'Report'
 
-    user        = models.ForeignKey(User)
-    reason      = models.CharField(max_length=400)
-    ex_id       = models.IntegerField(default=0)
-    ex_name     = models.CharField(max_length=16)
-    posted      = models.DateTimeField('date published')
+    user                = models.ForeignKey(User)
+    reason              = models.CharField(max_length=400)
+    ex_id               = models.IntegerField(default=0)
+    ex_name             = models.CharField(max_length=16)
+    posted              = models.DateTimeField('date published')
 
 class Comments(models.Model):
 
     class Meta:
         verbose_name = 'Comment'
 
-    user        = models.ForeignKey(User)
-    message     = models.CharField(max_length=400)
-    ex_id       = models.IntegerField(default=0)
-    ex_name     = models.CharField(max_length=16)
-    posted      = models.DateTimeField('date published')
+    user                = models.ForeignKey(User)
+    message             = models.CharField(max_length=400)
+    ex_id               = models.IntegerField(default=0)
+    ex_name             = models.CharField(max_length=16)
+    posted              = models.DateTimeField('date published')
 
 class Screenshots(models.Model):
 
     class Meta:
         verbose_name = 'Screenshot'
 
-    user        = models.ForeignKey(User)
-    ex_id       = models.IntegerField(default=0)
-    ex_name     = models.CharField(max_length=16)
-    posted      = models.DateTimeField('date published')
-    map_preview = models.BooleanField(default=False)
+    user                = models.ForeignKey(User)
+    ex_id               = models.IntegerField(default=0)
+    ex_name             = models.CharField(max_length=16)
+    posted              = models.DateTimeField('date published')
+    map_preview         = models.BooleanField(default=False)
