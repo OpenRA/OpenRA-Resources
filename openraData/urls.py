7acb64be26134a94f0338e62f80b71194ef036a8
Igor Popov <ihptru@gmail.com>
2013-12-27 00:03:10 +0400

    fix up urls.py

--- openraData/urls.py
+++ openraData/urls.py
@@ -9,24 +9,18 @@ from openraData import api
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     
-    url(r'^maps$', views.maps, name='maps'),
-    url(r'^maps/$', views.maps, name='maps'),
-    url(r'^units$', views.units, name='units'),
-    url(r'^units/$', views.units, name='units'),
-    url(r'^mods$', views.mods, name='mods'),
-    url(r'^mods/$', views.mods, name='mods'),
+    url(r'^maps/?$', views.maps, name='maps'),
+    url(r'^units/?$', views.units, name='units'),
+    url(r'^mods/?$', views.mods, name='mods'),
     
-    url(r'^login$', views.loginView, name='loginView'),
-    url(r'^login/$', views.loginView, name='loginView'),
-    url(r'^logout$', views.logoutView, name='logoutView'),
-    url(r'^logout/$', views.logoutView, name='logoutView'),
+    url(r'^login/?$', views.loginView, name='loginView'),
+    url(r'^logout/?$', views.logoutView, name='logoutView'),
     
     url(r'^login/register/?$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),
         name='registration_register'), 
     url(r'^login/', include('registration.backends.default.urls')),
-    
-    url(r'^panel$', views.ControlPanel, name='ControlPanel'),
-    url(r'^panel/$', views.ControlPanel, name='ControlPanel'),
+
+    url(r'^panel/?$', views.ControlPanel, name='ControlPanel'),
     
     url(r'^map/(?P<arg>\w+)/$', api.mapAPI, name='mapAPI_download'),
     url(r'^map/(?P<arg>\w+)/(?P<value>\w+)/$', api.mapAPI, name='mapAPI'), 
