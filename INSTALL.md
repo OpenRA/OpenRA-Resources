dd7ea60ec469f46d593e56fec0523e77c3ea73f0
Igor Popov <ihptru@gmail.com>
2014-02-15 23:30:27 +0400

    tidy up

--- INSTALL.md
+++ INSTALL.md
@@ -59,10 +59,10 @@ python manage.py syncdb
 ### Setup Production (using nginx + gunicorn)
 All-sufficient guide: http://goodcode.io/blog/django-nginx-gunicorn/
 
-Running gunicorn (WSGI HTTP Server) this way (10 instances, max timeout 120 seconds):
+Running gunicorn (WSGI HTTP Server) this way (10 instances, max timeout 30 seconds):
 
 ```
-gunicorn_django -w 10 -t 120 --log-file=/path/to/djangoserver.log -b 127.0.0.1:8000
+gunicorn_django -w 10 -t 30 --log-file=/path/to/djangoserver.log -b 127.0.0.1:8000
 ```
 
 Nginx config for our virtual host (replace PATH where needed):
--- openraData/views.py
+++ openraData/views.py
@@ -62,12 +62,12 @@ def ControlPanel(request):
     return StreamingHttpResponse(template.render(context))
 
 def maps(request, page=1, filter=""):
-    perPage = 10
+    perPage = 20
     slice_start = perPage*int(page)-perPage
     slice_end = perPage*int(page)
     mapObject = Maps.objects.all().annotate(count_hashes=Count("map_hash")).order_by("-posted").filter(next_rev=0)
     amount = len(mapObject)
-    rowsRange = int(math.ceil(amount/float(perPage)))   # amount of pages
+    rowsRange = int(math.ceil(amount/float(perPage)))   # amount of rows
     mapObject = mapObject[slice_start:slice_end]
     if len(mapObject) == 0 and int(page) != 1:
         return HttpResponseRedirect("/maps/")
