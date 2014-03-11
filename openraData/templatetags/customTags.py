2425ee5073b626a9758ec5228e670dea0534e9c6
Igor Popov <ihptru@gmail.com>
2014-03-11 12:10:53 +0400

    fix #150;

--- openraData/templatetags/customTags.py
+++ openraData/templatetags/customTags.py
@@ -1,5 +1,27 @@
+import re
+from HTMLParser import HTMLParser
 from django import template
 
 register = template.Library()
 
-#register.filter('name', name)
+def convert_links(value):
+	value = re.sub(r'(?P<urlmatch>https?:\/\/[^ ][^<][^\n][^\r]*)', '<a href="\g<urlmatch>" target=_blank>\g<urlmatch></a>', value)
+	return value
+
+register.filter('convert_links', convert_links)
+
+class MLStripper(HTMLParser):
+	def __init__(self):
+		self.reset()
+		self.fed = []
+	def handle_data(self, d):
+		self.fed.append(d)
+	def get_data(self):
+		return ''.join(self.fed)
+
+def strip_tags(value):
+	s = MLStripper()
+	s.feed(value)
+	return s.get_data()
+
+register.filter('strip_tags', strip_tags)
