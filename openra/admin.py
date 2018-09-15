955fb681f1b2b682b76f90418b13da4b3b192769
Paul Chote <pchote@users.noreply.github.com>
2018-09-15 07:58:20 -0700

    Remove Groups from the admin page.

--- openra/admin.py
+++ openra/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 from openra.models import Maps
 from openra.models import MapCategories
@@ -86,3 +86,6 @@ UserAdmin.list_display = list(UserAdmin.list_display) + ['date_joined']
 UserAdmin.ordering = ('-date_joined',)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+# Unregister unused models
+admin.site.unregister(Group)
