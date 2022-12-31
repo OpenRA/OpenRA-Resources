3096c3f6018e402e561b1dd8938f02fb4f3ee845
Dean Simmons <dsimmonsuk@gmail.com>
2022-12-31 02:53:44 +0000

    Initial docker integration

---
+++ openra/management/commands/testdocker.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
+from openra.services.docker import Docker
+
+class Command(BaseCommand):
+    help = 'Checks that docker is functioning as needed'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+
+        print(Docker().testDocker())
