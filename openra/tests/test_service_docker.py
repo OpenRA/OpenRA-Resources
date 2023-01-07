0b72c8ef0afb85fd8af95f696ac6f00c93f63fa1
Dean Simmons <dsimmonsuk@gmail.com>
2023-01-07 20:05:42 +0000

    Added result library and implemented in docker service

--- openra/tests/test_service_docker.py
+++ openra/tests/test_service_docker.py
@@ -1,17 +1,11 @@
-import datetime
-
-from django.utils import timezone
 from unittest import TestCase
 from unittest.mock import  Mock, MagicMock
 
-from docker.errors import ContainerError
-from openra.services.docker import Docker, IncompatibleAppImagePathException
-from openra.fakes import log
+from openra.services.docker import Docker, ErrorDockerExceptionResponse, ErrorDockerIncompatibleAppImagePath, ErrorDockerNonByteResponse
 from os import path
 from django.conf import settings
 
 class TestServiceDocker(TestCase):
-
     def test_that_test_docker_will_pass_the_correct_parameters(self):
         client_mock = Mock()
         client_mock.containers = Mock()
@@ -27,7 +21,7 @@ class TestServiceDocker(TestCase):
 
         self.assertEquals(
             'mock_return',
-            docker.test_docker()
+            docker.test_docker().unwrap()
         )
 
         client_mock.images.get.assert_called_once_with(settings.DOCKER_IMAGE_TAG)
@@ -57,7 +51,7 @@ class TestServiceDocker(TestCase):
             docker.extract_appimage(
                 '/sample/image.AppImage',
                 '/extract/to/location',
-            )
+            ).unwrap()
         )
 
         client_mock.images.get.assert_called_once_with(settings.DOCKER_IMAGE_TAG)
@@ -65,6 +59,7 @@ class TestServiceDocker(TestCase):
         client_mock.containers.run.assert_called_once_with(
             'fake_image',
             'bash -c "cp /in/AppImage . && '
+                    'chmod +x AppImage && '
                     './AppImage --appimage-extract && '
                     'rm AppImage"',
             remove=True,
@@ -92,9 +87,9 @@ class TestServiceDocker(TestCase):
         self.assertEquals(
             'mock_return',
             docker.extract_appimage(
-                b'/tmp/tmpkuld_787__tempfs__/appImage',
+                '/tmp/tmpkuld_787__tempfs__/appImage',
                 '/extract/to/location',
-            )
+            ).unwrap()
         )
 
     def test_extract_appimage_will_throw_exception_if_an_incompatible_filename_is_used(self):
@@ -102,11 +97,14 @@ class TestServiceDocker(TestCase):
 
         docker = Docker(client_mock)
 
-        self.assertRaises(
-            IncompatibleAppImagePathException,
-            docker.extract_appimage,
+        output = docker.extract_appimage(
             '/sample/im age.App_image',
             '/extract/to/location'
+        )
+
+        self.assertIsInstance(
+            output.unwrap_err(),
+            ErrorDockerIncompatibleAppImagePath
         )
 
     def test_run_utility_command_will_pass_the_correct_parameters(self):
@@ -133,7 +131,7 @@ class TestServiceDocker(TestCase):
                 [
                     'src/icons:icons'
                 ]
-            )
+            ).unwrap()
         )
 
         client_mock.images.get.assert_called_once_with(settings.DOCKER_IMAGE_TAG)
@@ -152,101 +150,3 @@ class TestServiceDocker(TestCase):
             ],
             working_dir="/build",
         )
-
-    def log_contains_non_bytes_message(self):
-        return log.contains('info', 'Docker returned non bytes response')
-
-    def test_that_it_will_log_an_entry_on_docker_run_if_docker_doesnt_return_bytes(self):
-        log.register()
-
-        client_mock = Mock()
-        client_mock.containers = Mock()
-        client_mock.images = Mock()
-        client_mock.images.get = MagicMock(
-            return_value ='fake_image'
-        )
-        client_mock.containers.run = MagicMock(
-            return_value = None
-        )
-
-        docker = Docker(client_mock)
-
-        self.assertFalse(self.log_contains_non_bytes_message())
-
-        self.assertEquals(
-            None,
-            docker.test_docker()
-        )
-
-        self.assertTrue(self.log_contains_non_bytes_message())
-
-        log.clear()
-
-        self.assertFalse(self.log_contains_non_bytes_message())
-
-        self.assertEquals(
-            None,
-            docker.extract_appimage('fake', 'fake')
-        )
-
-        self.assertTrue(self.log_contains_non_bytes_message())
-
-        log.clear()
-
-        self.assertFalse(self.log_contains_non_bytes_message())
-
-        self.assertEquals(
-            None,
-            docker.run_utility_command('fake', 'fake', [])
-        )
-
-        self.assertTrue(self.log_contains_non_bytes_message())
-
-    def log_contains_exception_message(self):
-        return log.contains('info', 'Docker has thrown an exception, most likely non-0 return')
-
-    def test_that_it_will_log_an_entry_on_docker_run_if_docker_throws_a_container_exception(self):
-        log.register()
-
-        client_mock = Mock()
-        client_mock.containers = Mock()
-        client_mock.images = Mock()
-        client_mock.images.get = MagicMock(
-            return_value ='fake_image'
-        )
-        client_mock.containers.run = MagicMock(
-            side_effect = Exception()
-        )
-
-        docker = Docker(client_mock)
-
-        self.assertFalse(self.log_contains_exception_message())
-
-        self.assertEquals(
-            None,
-            docker.test_docker()
-        )
-
-        self.assertTrue(self.log_contains_exception_message())
-
-        log.clear()
-
-        self.assertFalse(self.log_contains_exception_message())
-
-        self.assertEquals(
-            None,
-            docker.extract_appimage('fake', 'fake')
-        )
-
-        self.assertTrue(self.log_contains_exception_message())
-
-        log.clear()
-
-        self.assertFalse(self.log_contains_exception_message())
-
-        self.assertEquals(
-            None,
-            docker.run_utility_command('fake', 'fake', [])
-        )
-
-        self.assertTrue(self.log_contains_exception_message())
