c9f5080805ab722aa581594bca8bd54d1cc25796
Dean Simmons <dsimmonsuk@gmail.com>
2023-01-15 00:27:07 +0000

    Factory workaroud for models that dont use proper foreign ids

--- openra/tests/test_factories.py
+++ openra/tests/test_factories.py
@@ -7,15 +7,15 @@ from . import factories
 
 class TestFactories(TestCase):
 
-    def test_it_can_create_models_from_all_factories(self):
-        factories.UserFactory()
-        factories.MapsFactory()
-        factories.MapCategoriesFactory()
-        factories.MapUpgradeLogsFactory()
-        factories.LintsFactory()
-        factories.CommentsFactory()
-        factories.UnsubscribeCommentsFactory()
-        factories.ReportsFactory()
-        factories.ScreenshotsFactory()
-        factories.RatingFactory()
-        factories.EngineFactory()
+    def test_it_can_create_saveable_models_from_all_factories(self):
+        factories.UserFactory().save()
+        factories.MapsFactory().save()
+        factories.MapCategoriesFactory().save()
+        factories.MapUpgradeLogsFactory().save()
+        factories.LintsFactory().save()
+        factories.CommentsFactory().save()
+        factories.UnsubscribeCommentsFactory().save()
+        factories.ReportsFactory().save()
+        factories.ScreenshotsFactory().save()
+        factories.RatingFactory().save()
+        factories.EngineFactory().save()
