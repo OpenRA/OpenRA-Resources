47b4e0209161d100f2ab67a1c29daeeab68fa99a
Dean Simmons <dsimmonsuk@gmail.com>
2023-09-03 10:02:22 +0100

    Undo hard coding fallback HTTP_HOST

--- openra/api.py
+++ openra/api.py
@@ -74,7 +74,7 @@ def __map_info_from_objects(request, map_objects, yaml):
         else:
             minimap = ''
 
-        download_url = 'http://' + request.META.get('HTTP_HOST', 'resource.openra.net') + \
+        download_url = 'http://' + request.META['HTTP_HOST'] + \
                        '/maps/' + str(map_object.id) + '/oramap'
 
         # TODO: Title and author have ' replaced with '' before insertion into the database. Work out why and fix it
